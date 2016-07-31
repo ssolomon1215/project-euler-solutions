dd1f6cd3be132c8901e4612c36bab034a5065657
Shawn Solomon <ssolomon1215@gmail.com>
2016-07-30 21:20:42 -0500

    Better Comments, more modular.
    Added new line at end of file. Updated both existing files with header
    comments. Updated both files' code to be a little more module.

--- euler-1.js
+++ euler-1.js
@@ -1,18 +1,19 @@
 /*
-// Spec:
+// https://projecteuler.net/problem=1
+//
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 //
 // Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
-function euler1(limit) 
+function euler1(target1, target2, limit) 
 {
-	let sum = 0;
+	var sum = 0;
 
 	for(x = 0; x < limit; x++) 
 	{
 		
-		if(x % 3 === 0 || x % 5 === 0) 
+		if(x % target1 === 0 || x % target2 === 0) 
 		{
 			sum += x;
 		}
@@ -24,4 +25,4 @@ function euler1(limit)
 	}
 }
 
-console.log(euler1(1000));
+console.log(euler1(3, 5, 1000));
